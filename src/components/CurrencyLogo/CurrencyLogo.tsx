2d886dd452e28b46f327ce43ba37712f05c875a4
webners1 <faizan_babar70@yahoo.com>
2024-06-24 21:20:26 +0500

    add

--- src/components/CurrencyLogo/CurrencyLogo.tsx
+++ src/components/CurrencyLogo/CurrencyLogo.tsx
@@ -115,8 +115,8 @@ const LOGO: Record<number, string> = {
   [ChainId.ARBITRUM_NOVA]: EthereumLogo,
   [ChainId.METIS]: MetisLogo,
   [ChainId.BOBA_AVAX]: BobaLogo,
-  [ChainId.ZETA]: ZetaLogo,
-  [ChainId.ZETA_TESTNET]: ZetaLogo,
+  // [ChainId.ZETA]: ZetaLogo,
+  // [ChainId.ZETA_TESTNET]: ZetaLogo,
 }
 
 export interface CurrencyLogoProps {
